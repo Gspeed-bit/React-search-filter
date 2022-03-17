ad9fea1a584707b47c307305eb2bb487969b6e8f
Ayodeji Daniel Adeyeye <adeyeye17@gmail.com>
2022-03-18 00:05:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
 import { useState } from 'react';
-import './App.css';
 import { Table } from './Table';
-import {Users} from "./Users";
+import { Users } from './Users';
+import './App.css';
+
 
 function App() {
 
-const [query, setQuery] = useState("")
+  const [query, setQuery] = useState("")
 
 
-function handleChange (event){
-setQuery(event.target.value)
+  console.log ()
+  const handleChange =(e) =>{
+    setQuery(e.target.value)
+  }
+
+const keys =["first_name", "last_name", "email"]
+
+const search = (data) =>{
+  return data.filter(item =>(
+    keys.some(key => item[key].toLowerCase().includes(query))
+
+  ))
+
+
 }
 
-const keys = ["first_name", "last_name", "email" ]
-
-const search = (data) => {
-  return data.filter(
-    (item)=>(
-
-  keys.some((key) => item[key].toLowerCase().includes(query))
-                 //or
-        // item.first_name.toLowerCase().includes(query)||
-        // item.last_name.toLowerCase().includes(query)||
-        // item.email.toLowerCase().includes(query)
-  )
-  )
-} 
-
 return (
-    <div className="App">
+  <div className="App">
       <header className="App-header">
-      <input
-      type="text"
-      className="text"
-      placeholder="Search here" 
-      onChange={handleChange}
-      />
-      
+  <input 
+  type="text" 
+  className='text'
+  placeholder='Search Here'
+  onChange={handleChange}
+  />
 
-      <Table  query={query} data = {search(Users)}/>
+<Table datas={search(Users)}/>
       </header>
     </div>
   );
--- src/Table.js
+++ src/Table.js
@@ -1,27 +1,28 @@
 import React from 'react'
 
-export const Table = ({data}) => {
+export const Table = ({datas}) => {
+
+
   return (
-<div> 
-<table className='tabel'>
-  <thead>
-    <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Email</th>
-    </tr>
-  </thead>
-  
-  <tbody>
-  {data.map((item) => (
-<tr key={item.id}>
-      <td>{item.first_name}</td>
-      <td>{item.last_name}</td>
-      <td>{item.email}</td>
-    </tr>
-    ))}
-  </tbody>
-</table>
+    <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Gmail</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            <tbody>
+           {datas.map((data) => ( 
+                 <tr key={data.id}>
+                <td>{`${data.first_name} ${data.last_name}`}</td>
+                <td>{data.email}</td>
+                <td>{data.gender}</td>
+                </tr>  
+           ))}  
+          </tbody>
+          </table>
     </div>
   )
 }
